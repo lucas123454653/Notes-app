5f4d2fe36f4b126d91b249496a8a76e8d2f31e28
lucas123454653 <162929460+lucas123454653@users.noreply.github.com>
2024-07-28 11:42:39 +1200

    HTTP Requests, and message flashing

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -13,8 +13,25 @@ def logout():
     return render_template("logout.html")
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method =='POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 2 characters.', category='error')
+        elif password1 != password2:
+             flash('Passwords don\'t match', category='error')
+        elif len(password1) <7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+            pass
     return render_template("signup.html")
 
 
