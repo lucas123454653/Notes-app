ed392c95c70ee45bc9fd6da6c1ce3ca9ff34ce80
lucas123454653 <162929460+lucas123454653@users.noreply.github.com>
2024-07-28 15:58:17 +1200

    Creating new user accounts

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
 
 auth = Blueprint('auth', __name__)
-
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -30,8 +33,12 @@ def sign_up():
         elif len(password1) <7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+            new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
-            pass
+            return redirect(url_for('views.home'))
+
     return render_template("signup.html")
 
 
