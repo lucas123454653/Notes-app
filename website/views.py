0bd25aec6a7eb0f94c330f113511a9a9efac79b6
lucas123454653 <162929460+lucas123454653@users.noreply.github.com>
2024-08-02 11:33:55 +1200

    Seperated home and notes page

--- website/views.py
+++ website/views.py
@@ -1,37 +1,42 @@
 from flask import Blueprint, render_template, request, flash, jsonify
-from flask_login import login_required, current_user
+from flask_login import login_user, login_required, logout_user, current_user
 from .models import Note
 from . import db
 import json
 
-views = Blueprint('views', __name__)
 
 
-@views.route('/', methods=['GET', 'POST'])
-@login_required
+
+views = Blueprint("views", __name__)
+
+
+@views.route("/", )
+@views.route("/home", )
 def home():
-    if request.method == 'POST': 
+    return render_template("home.html",user=current_user)
+
+
+@views.route("/notes", methods=['GET', 'POST'])
+def notes():
+    if request.method == 'POST':
         note = request.form.get('note')
-
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
+        if len(note) < 1: 
+             flash('Note is too short!', category='error')
         else:
-            new_note = Note(data=note, user_id=current_user.id)  
-            db.session.add(new_note)  
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
             db.session.commit()
-            flash('Note added!', category='success')
+            flash('Note added:', category='success')
 
-    return render_template("home.html", user=current_user)
+    return render_template("notes.html",user=current_user)
 
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) 
+@views.route('/delete-note' , methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
         if note.user_id == current_user.id:
             db.session.delete(note)
             db.session.commit()
-
-    return jsonify({})
+    return jsonify ({})
