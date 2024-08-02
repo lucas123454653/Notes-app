caebc764a7ddaa345236591344deab943d8e38f5
lucas123454653 <162929460+lucas123454653@users.noreply.github.com>
2024-08-02 19:35:23 +1200

    PEP8 Formating

--- website/__init__.py
+++ website/__init__.py
@@ -20,7 +20,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
-    
+
     with app.app_context():
         db.create_all()
 
--- website/views.py
+++ website/views.py
@@ -5,32 +5,31 @@ from . import db
 import json
 
 
-
-
 views = Blueprint("views", __name__)
 
 
 @views.route("/", )
 @views.route("/home", )
 def home():
-    return render_template("home.html",user=current_user)
+    return render_template("home.html", user=current_user)
 
 
 @views.route("/notes", methods=['GET', 'POST'])
 def notes():
     if request.method == 'POST':
         note = request.form.get('note')
-        if len(note) < 1: 
-             flash('Note is too short!', category='error')
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
         else:
             new_note = Note(data=note, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
             flash('Note added:', category='success')
 
-    return render_template("notes.html",user=current_user)
+    return render_template("notes.html", user=current_user)
 
-@views.route('/delete-note' , methods=['POST'])
+
+@views.route('/delete-note', methods=['POST'])
 def delete_note():
     note = json.loads(request.data)
     noteId = note['noteId']
@@ -39,5 +38,5 @@ def delete_note():
         if note.user_id == current_user.id:
             db.session.delete(note)
             db.session.commit()
-            
-    return jsonify ({})
+
+    return jsonify({})
